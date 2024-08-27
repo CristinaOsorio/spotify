ab0d0282352d5af26e5e6e967971679bec4e48b8
CristinaOsorio <macry_08@hotmail.com>
2024-08-27 12:14:59 -0600

    feat: apply order-list pipe functionality and implement in play-list-body

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -3,16 +3,33 @@ import * as dataRaw from '../../../data/tracks.json';
 import { Tracks } from '../../../core/models/tracks.model';
 
 @Component({
-  selector: 'app-play-list-body',
-  templateUrl: './play-list-body.component.html',
-  styleUrls: ['./play-list-body.component.css'],
+    selector: 'app-play-list-body',
+    templateUrl: './play-list-body.component.html',
+    styleUrls: ['./play-list-body.component.css'],
 })
 export class PlayListBodyComponent implements OnInit {
-  tracks: Array<Tracks> = [];
-  constructor() {}
+    tracks: Array<Tracks> = [];
+    orderOption: {
+        property: 'name' | 'album' | null;
+        order: 'asc' | 'desc';
+    } = {
+        property: null,
+        order: 'asc',
+    };
 
-  ngOnInit(): void {
-    const { data } = (dataRaw as any).default;
-    this.tracks = data;
-  }
+    constructor() {}
+
+    ngOnInit(): void {
+        const { data } = (dataRaw as any).default;
+        this.tracks = data;
+    }
+
+    changeSort(property: 'name' | 'album' | null): void {
+        const { order } = this.orderOption;
+
+        this.orderOption = {
+            property,
+            order: order == 'asc' ? 'desc' : 'asc',
+        };
+    }
 }
--- src/app/shared/pipes/order-list.pipe.ts
+++ src/app/shared/pipes/order-list.pipe.ts
@@ -1,11 +1,35 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { TracksModule } from '../../modules/tracks/tracks.module';
+import { Tracks } from './../../core/models/tracks.model';
 
 @Pipe({
-  name: 'orderList',
+    name: 'orderList',
 })
 export class OrderListPipe implements PipeTransform {
-  transform(value: TracksModule[]): Array<any> {
-    return value;
-  }
+    transform(
+        value: any[],
+        args: string | null = null,
+        sort: 'asc' | 'desc' = 'asc'
+    ): Tracks[] {
+        try {
+            if (args === null) {
+                return value;
+            }
+
+            const orderTrack = value.sort((a, b) => {
+                if (a[args] < b[args]) {
+                    return -1;
+                } else if (a[args] > b[args]) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+
+            return sort == 'asc' ? orderTrack : orderTrack.reverse();
+        } catch (error) {
+            console.log(error);
+
+            return [];
+        }
+    }
 }
