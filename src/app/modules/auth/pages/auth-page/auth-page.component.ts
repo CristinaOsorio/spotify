1ab5d48ea1e1c97302b6d4176142840596ce2ba3
CristinaOsorio <macry_08@hotmail.com>
2024-08-27 22:57:25 -0600

    feat: add login view with reactive form

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-auth-page',
-  templateUrl: './auth-page.component.html',
-  styleUrls: ['./auth-page.component.css']
+    selector: 'app-auth-page',
+    templateUrl: './auth-page.component.html',
+    styleUrls: ['./auth-page.component.css'],
 })
 export class AuthPageComponent implements OnInit {
+    formLogin = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [
+            Validators.required,
+            Validators.minLength(5),
+        ]),
+    });
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    sendLogin() {
+        console.log(this.formLogin.value);
+        this.formLogin.reset();
+    }
 }
