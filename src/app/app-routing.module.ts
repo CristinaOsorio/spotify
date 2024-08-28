b5856300ceee6760f44c4b25b273633a11f1b0e3
CristinaOsorio <macry_08@hotmail.com>
2024-08-28 15:47:03 -0600

    feat: protect route with SessionGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
+import { SessionGuard } from '@core/guards/session/session.guard';
 
 const routes: Routes = [
-  {
-    path: 'auth',
-    loadChildren: () =>
-      import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  },
-  {
-    path: '',
-    component: HomePageComponent,
-    loadChildren: () =>
-      import('./modules/home/home.module').then((m) => m.HomeModule),
-  },
+    {
+        path: 'auth',
+        loadChildren: () =>
+            import('./modules/auth/auth.module').then((m) => m.AuthModule),
+    },
+    {
+        path: '',
+        component: HomePageComponent,
+        loadChildren: () =>
+            import('./modules/home/home.module').then((m) => m.HomeModule),
+        canActivate: [SessionGuard],
+    },
+    {
+        path: '**',
+        redirectTo: 'auth',
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
