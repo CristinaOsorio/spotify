7f4e4640fc70a1038ab12c7288562bf32b3e2781
CristinaOsorio <macry_08@hotmail.com>
2024-08-28 11:52:06 -0600

    feat: emit selected card player to media player

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,22 +1,27 @@
+import { MultimediaService } from './../../services/multimedia/multimedia.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Tracks } from '../../../core/models/tracks.model';
 
 @Component({
-  selector: 'app-card-player',
-  templateUrl: './card-player.component.html',
-  styleUrls: ['./card-player.component.css'],
+    selector: 'app-card-player',
+    templateUrl: './card-player.component.html',
+    styleUrls: ['./card-player.component.css'],
 })
 export class CardPlayerComponent implements OnInit {
-  @Input() mode: 'small' | 'big' = 'small';
-  @Input() track: Tracks = {
-    name: '',
-    album: '',
-    cover: '',
-    url: '',
-    _id: '',
-  };
+    @Input() mode: 'small' | 'big' = 'small';
+    @Input() track: Tracks = {
+        name: '',
+        album: '',
+        cover: '',
+        url: '',
+        _id: '',
+    };
 
-  constructor() {}
+    constructor(private multimediaService: MultimediaService) {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
+
+    sendPlay() {
+        this.multimediaService.callback.emit(this.track);
+    }
 }
--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,22 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { MultimediaService } from './../../services/multimedia/multimedia.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Tracks } from '../../../core/models/tracks.model';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-media-player',
-  templateUrl: './media-player.component.html',
-  styleUrls: ['./media-player.component.css'],
+    selector: 'app-media-player',
+    templateUrl: './media-player.component.html',
+    styleUrls: ['./media-player.component.css'],
 })
-export class MediaPlayerComponent implements OnInit {
-  mockCover: Tracks = {
-    cover:
-      'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
-    name: 'Name del artista',
-    album: 'Tema del album',
-    url: 'url',
-    _id: 1,
-  };
+export class MediaPlayerComponent implements OnInit, OnDestroy {
+    callback$!: Subscription;
 
-  constructor() {}
+    mockCover: Tracks = {
+        cover: 'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
+        name: 'Name del artista',
+        album: 'Tema del album',
+        url: 'url',
+        _id: 1,
+    };
 
-  ngOnInit(): void {}
+    constructor(private multimediaService: MultimediaService) {}
+
+    ngOnInit(): void {
+        this.callback$ = this.multimediaService.callback.subscribe(
+            (response) => {
+                this.mockCover = response;
+            }
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.callback$.unsubscribe();
+    }
 }
