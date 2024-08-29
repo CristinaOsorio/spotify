d503108e40f1d99c16e62cc5097fb9ccf4c0fce0
CristinaOsorio <macry_08@hotmail.com>
2024-08-29 16:49:31 -0600

    test: add mock request simulation test in service

--- src/app/modules/auth/services/auth.service.spec.ts
+++ src/app/modules/auth/services/auth.service.spec.ts
@@ -2,18 +2,51 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AuthService } from './auth.service';
+import * as mockRaw from '../../../data/user.json';
+import { of } from 'rxjs';
 
 describe('AuthService', () => {
     let service: AuthService;
+    let mockUser = (mockRaw as any).default;
+    let httpClientSpy: {
+        post: jasmine.Spy;
+    };
+    let cookieService: {
+        set: jasmine.Spy;
+    };
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
         });
-        service = TestBed.inject(AuthService);
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+        cookieService = jasmine.createSpyObj('CookieService', ['set']);
+        service = new AuthService(httpClientSpy as any, cookieService as any);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should return a objet with data and tokenSession', (done: DoneFn) => {
+        // Arrange
+        const userMock = mockUser.userOk;
+        const mockResponse = {
+            data: {},
+            tokenSession: 'token',
+        };
+
+        // Assest
+        httpClientSpy.post.and.returnValue(of(mockResponse));
+
+        // Act
+        service
+            .sendCredencial(userMock.email, userMock.password)
+            .subscribe((responseApi) => {
+                const getProperties = Object.keys(responseApi);
+                expect(getProperties).toContain('data');
+                expect(getProperties).toContain('tokenSession');
+                done();
+            });
     });
 });
