2d621fcaf2526e6fa77c5ca950a90b3445e76d10
CristinaOsorio <macry_08@hotmail.com>
2024-08-29 22:23:59 -0600

    feat: delete modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
 import { SessionGuard } from '@core/guards/session/session.guard';
 
-const routes: Routes = [
+export const appRoutes: Routes = [
     {
         path: 'auth',
         loadChildren: () =>
-            import('./modules/auth/auth.module').then((m) => m.AuthModule),
+            import('./modules/auth/auth.routes').then((m) => m.authRoutes),
     },
     {
         path: '',
         component: HomePageComponent,
         loadChildren: () =>
-            import('./modules/home/home.module').then((m) => m.HomeModule),
+            import('./modules/home/home.routes').then((m) => m.homeRoutes),
         canActivate: [SessionGuard],
     },
     {
@@ -21,9 +20,3 @@ const routes: Routes = [
         redirectTo: 'auth',
     },
 ];
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-})
-export class AppRoutingModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,27 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { InjectSessionInterceptor } from '@core/interceptors/inject-session/inject-session.interceptor';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import {
+    HTTP_INTERCEPTORS,
+    withInterceptorsFromDi,
+    provideHttpClient,
+} from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideRouter(appRoutes),
+        importProvidersFrom(BrowserModule),
         CookieService,
         {
             provide: HTTP_INTERCEPTORS,
@@ -24,6 +29,5 @@ bootstrapApplication(AppComponent, {
             multi: true,
         },
         provideHttpClient(withInterceptorsFromDi()),
-    ]
-})
-  .catch(err => console.error(err));
+    ],
+}).catch((err) => console.error(err));
