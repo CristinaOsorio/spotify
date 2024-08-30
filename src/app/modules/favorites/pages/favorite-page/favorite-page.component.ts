7d846090b10c83acae4f1d00df272fcba2882002
CristinaOsorio <macry_08@hotmail.com>
2024-08-29 18:06:18 -0600

    feat: add functionality to move track to favorite tracks

--- src/app/modules/favorites/pages/favorite-page/favorite-page.component.ts
+++ src/app/modules/favorites/pages/favorite-page/favorite-page.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Tracks } from '@core/models/tracks.model';
+import { TrackService } from '@modules/tracks/services/track.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-favorite-page',
-  templateUrl: './favorite-page.component.html',
-  styleUrls: ['./favorite-page.component.css'],
+    selector: 'app-favorite-page',
+    templateUrl: './favorite-page.component.html',
+    styleUrls: ['./favorite-page.component.css'],
 })
 export class FavoritePageComponent implements OnInit {
-  constructor() {}
+    tracks$!: Observable<Tracks[]>;
+    constructor(private trackService: TrackService) {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {
+        this.tracks$ = this.trackService.getAllRandom$();
+    }
 }
